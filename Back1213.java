89dacbb6309284652f3f2bfec5fa69bcd75bf3fa
고성태 <v12rhtjdxo@naver.com>
2024-04-21 19:28:11 +0900

    240421

---
+++ Back1213.java
@@ -0,0 +1,47 @@
+package algorithm_problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back1213 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] arr = new int[27];
+        for (int i = 0; i < str.length(); i++) {
+            arr[(int) str.charAt(i) - 65]++;
+        }
+
+        int checker = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                checker++;
+            }
+        }
+
+        String a = "";
+        String b = "";
+        String c = "";
+
+        if (checker >= 2) {
+            System.out.println("I'm Sorry Hansoo");
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i] / 2; j++) {
+                    a += (char) (i + 65);
+                }
+
+                for (int j = 0; j < arr[arr.length - (i + 1)] / 2; j++) {
+                    c += (char) (arr.length - (i + 1) + 65);
+                }
+
+                if (arr[i] % 2 != 0) {
+                    b += (char) (i + 65);
+                }
+            }
+
+            System.out.println(a + b + c);
+        }
+    }
+}
