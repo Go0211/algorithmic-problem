14c215986f551459bc71575049661a4b561219f8
고성태 <v12rhtjdxo@naver.com>
2024-04-22 23:45:06 +0900

    240422

---
+++ Back1969.java
@@ -0,0 +1,45 @@
+package algorithm_problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back1969 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strArr = br.readLine().split(" ");
+        int seq = Integer.parseInt(strArr[0]);
+        int len = Integer.parseInt(strArr[1]);
+        String[] arr = new String[seq];
+        int[] idxArr;
+        String str = "";
+        int total = 0;
+
+        for (int i = 0; i < seq; i++) {
+            arr[i] = br.readLine();
+        }
+
+        for (int i = 0; i < len; i++) {
+            idxArr = new int[26];
+            int idx = 0;
+
+            for (int j = 0; j < arr.length; j++) {
+                idxArr[arr[j].charAt(i) - 65]++;
+            }
+
+            for (int j = 1; j < idxArr.length; j++) {
+                if (idxArr[idx] < idxArr[j]) {
+                    total += idxArr[idx];
+                    idx = j;
+                    continue;
+                }
+                total += idxArr[j];
+            }
+
+            str += (char)(idx + 65);
+        }
+
+        System.out.println(str);
+        System.out.println(total);
+    }
+}
---
+++ Back2193.java
@@ -0,0 +1,30 @@
+package algorithm_problem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back2193 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        long[] dp = new long[91];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i < dp.length; i++) {
+            for (int j = 0; j < i - 1; j++) {
+                dp[i] += dp[j];
+            }
+        }
+
+        System.out.println(dp[num]);
+
+//        1 -> 1
+//        2 -> 10
+//        3 -> 100, 101
+//        4 -> 1000, 1001, 1010
+//        5 -> 10000, 10101, 10100, 10001, 10010
+    }
+}
